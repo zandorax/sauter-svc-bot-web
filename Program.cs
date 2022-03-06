c8db00b2601603aa3875f33011ca4242c5c3e187
Dominic Blattner <zandorax@protonmail.com>
2022-03-06 21:37:13 +0100

    signalR example

--- Program.cs
+++ Program.cs
@@ -1,11 +1,16 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using SauterBot_connector;
+using SauterSvcBotWeb;
+//For testing
+using Microsoft.AspNetCore.ResponseCompression;
+using SauterSvcBotWeb.Server.Hubs;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+
 
 await builder.Build().RunAsync();
